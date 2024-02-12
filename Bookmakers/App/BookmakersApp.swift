d587062508c127cc4d356658a36dad4522d787c0
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2024-02-12 11:11:48 +0300

    Add project

---
+++ Bookmakers/App/BookmakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmakersApp.swift
+//  Bookmakers
+//
+//  Created by Александр Косяков on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookmakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView().preferredColorScheme(.light)
+        }
+    }
+}
